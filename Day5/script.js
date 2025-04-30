c98be08de4f06c03207034f86534ed53cef5728d
Prajktac210 <prajktac210@gmail.com>
2025-04-30 16:28:32 +0530

    change file name

--- Day5/script.js
+++ Day5/script.js
@@ -33,31 +33,3 @@ e %= 6
 console.log(e)
 
 
-let p = 46
-let q = 24
-
-console.log(p != q)
-
-
-let OTP = 1234
-
-let USER_OTP = "1234"
-
-console.log(USER_OTP === OTP )
-
-let axy = 10 ;
-
-let byz = 20;
-
-let result =  10 < 20;
-
-console.log(result)
-
-
-let res = 12 >= 12
-
-console.log(res)
-
-let res1 =  12 <= 12
-
-console.log(res1)
