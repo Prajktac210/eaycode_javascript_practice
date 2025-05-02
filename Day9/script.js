cc0ab22a5486e307d77a8e05fa364ee44fe11be0
Prajktac210 <prajktac210@gmail.com>
2025-05-02 13:55:35 +0530

    add day 9 and 10

---
+++ Day9/script.js
@@ -0,0 +1,17 @@
+let num = 6
+
+if (num %2== 0) {
+ console.log("number is even")
+ } else {
+    console.log("number is odd");
+ }
+
+
+ let age = 21;
+
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not yet eligible to vote.");
+  
+}
