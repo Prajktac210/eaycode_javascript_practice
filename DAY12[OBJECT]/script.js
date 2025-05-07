7966997fef7bcff02e5b6e0c6f4b769debcfef11
Prajktac210 <prajktac210@gmail.com>
2025-05-07 21:38:01 +0530

    learn about object

---
+++ DAY12[OBJECT]/script.js
@@ -0,0 +1,36 @@
+let mydata = {
+    name : "prajkta",
+    age : 21,
+    address :"shirwal"
+}
+console.log(mydata)
+
+let MyInstaData = {
+    
+        is_private : true,
+       accunt_name : "prajktavdchavan27",
+         user_name : "prajkta chavan",
+    
+   following:{
+          "1": "sarthak",
+         "2" :"easyCode"
+     },
+    
+    followers : {
+            "1": "sarthak",
+          "2" :"easyCode"
+       },
+    
+       posts : [
+          {
+             url: "myphoto.jpg",
+             likes : 100,
+          date: "1/3/2024"
+       }
+      ]
+     }
+    
+    
+    
+   console.log(MyInstaData )
+  
