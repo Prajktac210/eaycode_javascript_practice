9687b22112f8d4d1c5e1fe8636cad91552692672
Prajktac210 <prajktac210@gmail.com>
2025-05-07 19:44:31 +0530

    learn array properties

---
+++ Day12[ARRAY PROPERTIES]/script.js
@@ -0,0 +1,34 @@
+let num=[1,2,3,4,5];
+console.log(num.reverse())
+
+let alpha=["a","b","c","d","e"]
+  console.log(alpha.reverse())
+
+  let student=["prajkta","trupti","pratiksha"];
+  console.log(student.reverse())
+
+
+
+ console.log(alpha.includes('c'))
+
+console.log(num.includes(4))
+
+
+console.log(alpha)
+console.log(num)
+
+
+let colors = ["red", "green", "blue", "yellow", "orange", "purple"];
+
+console.log(colors.indexOf("pink"));
+
+
+let cities = ["pune","satara","mumbai","dellhi"];
+
+
+
+ console.log(cities.sort())
+
+
+
+console.log(cities.toString());
