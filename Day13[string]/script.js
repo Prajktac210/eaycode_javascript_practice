861dae0989e13dde85b292821ff546ddb4448ec8
Prajktac210 <prajktac210@gmail.com>
2025-05-08 21:32:47 +0530

    learn about string and it's method

---
+++ Day13[string]/script.js
@@ -0,0 +1,14 @@
+let name="Prajkta vidyadilip chavan"
+let address="shirwal"
+let state="maharashtra"
+console.log(name.length)//find out length
+console.log(name.toUpperCase()) //convert into uppercase
+console.log(name.toLowerCase()) //convert into lowercase
+console.log("my name is "+ name +"live at"+address)
+
+console.log(address.concat(state))
+
+console.log(name.split(""))// display each and every character is seprate
+
+let sentence="itamtlivetintatshekhamirewadi"  // create a space after a specific character
+console.log(sentence.split("t"))
