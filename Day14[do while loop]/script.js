e6186d9e3fe9fefc9861268cf4c6aa3f90dec2fa
Prajktac210 <prajktac210@gmail.com>
2025-05-09 21:46:51 +0530

    learn do while loop

---
+++ Day14[do while loop]/script.js
@@ -0,0 +1,18 @@
+let num=1;
+do{
+console.log(num);
+num++;
+}while(num<10)
+
+
+
+    let x=0;
+do{
+console.log("hello guys",x);
+x++;
+}while(x<=20)
+
+
+    
+
+
---
+++ Day14{while loop]/script.js
@@ -0,0 +1,22 @@
+let num=0;
+while(num<10){
+    console.log(num);
+    num++;
+}
+
+// let n=1;
+// while(n<=100){
+//     console.log("even number");
+//     if(n%2==0){
+//         console.log(n);
+
+//     }
+//     n++
+// }
+
+let table=Number(prompt("enter a number")) ;
+let i=1;
+while(i<=10){
+  document.writeln(table  + "*" +i+"=" +table*i+"<br>")
+    i++;
+}
